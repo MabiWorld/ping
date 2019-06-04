c75adb200aee23d0a058d45498d24ada70d5074c
Sapphire Becker <github@logicplace.com>
2019-06-04 09:41:21 -0700

    Update Alexina IP

--- z-copy-paster.ps1
+++ z-copy-paster.ps1
@@ -1,6 +1,6 @@
 while (1) {
 	(Measure-Command {
-		$sock = new-object System.Net.Sockets.TcpClient("208.85.109.47", 11020)
+		$sock = new-object System.Net.Sockets.TcpClient("52.32.149.152", 11020)
 		(new-object System.IO.StreamReader($sock.GetStream())).Read([char[]]::new(4), 0, 4) | Out-Null
 		$sock.Close()
 	}).Milliseconds
