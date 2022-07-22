90ef4cedc665e12414c6f0ba7f1285098cfd3265
Sapphire Becker <github@logicplace.com>
2022-07-22 11:50:27 -0700

    use erinn ch1 IP

--- z-copy-paster.ps1
+++ z-copy-paster.ps1
@@ -1,6 +1,6 @@
 while (1) {
 	(Measure-Command {
-		$sock = new-object System.Net.Sockets.TcpClient("34.208.22.139", 11020)
+		$sock = new-object System.Net.Sockets.TcpClient("34.218.42.114", 11020)
 		(new-object System.IO.StreamReader($sock.GetStream())).Read([char[]]::new(4), 0, 4) | Out-Null
 		$sock.Close()
 	}).Milliseconds
